9abb8e66f4e8f1f86d203463fa35a14caffb635c
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-02-23 16:58:18 +0100

    feat: add button to handle new product to add (just a redirect for now)

--- app/pages/products/page.tsx
+++ app/pages/products/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { TitleType } from '@models/Title'
 import { Title } from '@components/Title'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function page() {
 
@@ -12,7 +14,24 @@ export default function page() {
 
   return (
     <div>
-        <Title {...title} />
+        <div className='flex items-center'>
+            <div className='mr-auto'>
+                <Title {...title} />
+            </div>
+
+            <Link href="/products/add">
+                <div className='flex items-center justify-center gap-2 mr-5 handle-product-button'>
+                        <Image
+                            src="/icons/reglages.svg"
+                            alt="Add Product"
+                            width={30}
+                            height={30}
+                            className='object-contain handle-product-image'
+                        />
+                        <p className='handle-product-text'>Gérer</p>
+                </div>
+            </Link>
+        </div>
     </div>
   )
 }
