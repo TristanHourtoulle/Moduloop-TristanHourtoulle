ad51b9d009b7ab963a3b80002dd3ee2e27cfbfb8
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-02-23 10:06:30 +0100

    feat: add session check in the navbar by using API route

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"
+import { useEffect, useState } from "react";
 
 const Header = () => {
-  return (
-    <div className='flex w-full py-6 mb-10'>
-        <p className="text">Header</p>
-    </div>
-  )
-}
+  const [session, setSession] = useState(null);
 
-export default Header
+  useEffect(() => {
+    const fetchSession = async () => {
+      const res = await fetch("/api/session", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log("data get: ", data)
+      setSession(data.session);
+    };
+
+    fetchSession();
+  }, []);
+
+  return (
+    session ? (
+      <div>Connected</div>
+    ) : (
+      <div>Not connected</div>
+    )
+  );
+};
+
+export default Header;
