9ed171cbd7eaa5c488801ca15d9f2caf94bfb72b
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-02-23 10:05:50 +0100

    feat: add session state on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { getSession } from "@lib/session";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getSession();
+  const username = " " + session?.user?.name || '';
+
   return (
     <div>
-      <h1>Bienvenue sur l'outil Moduloop !</h1>
+      <h1>Bonjour{username}, Bienvenue sur l'outil Moduloop !</h1>
     </div>
   );
 }
