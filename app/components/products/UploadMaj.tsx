0f696ad4afc6dcee877eb1e373f5d3d3060308b8
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-02-26 15:23:49 +0100

    feat: update upload form to store in db the add value

--- app/components/products/UploadMaj.tsx
+++ app/components/products/UploadMaj.tsx
@@ -1,12 +1,35 @@
 "use client"
 
+import { ProductType } from "@models/Product";
 import { useState } from "react";
+
+
+async function addData(data: JSON) {
+    try {
+        const parseData = JSON.parse(data);
+        const addSection = parseData.Add;
+        addSection.forEach(async (element: any) => {
+            const res = await fetch('/api/product', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(element)
+            });
+            if (!res.ok) throw new Error(await res.text());
+            const updateData = await res.json();
+        });
+        return { success: true, message: "Data added" };
+    } catch (e: any) {
+        return { success: false, message: e.message };
+    }
+}
 
 export default function UploadForm() {
     const [file, setFile] = useState(null);
 
     const onSubmit = async (e) => {
-        e.preventDefault;
+        e.preventDefault();
         if (!file) return
 
         try {
@@ -17,10 +40,16 @@ export default function UploadForm() {
                 method: 'POST',
                 body: data
             })
-
             if (!res.ok) throw new Error(await res.text())
+            const updateData = await res.json();
+
+            if (!updateData.success) {
+                return ("KO: " + updateData.message);
+            }
+            addData(updateData.data);
         } catch (e) {
             console.log("ERROR: ", e)
+            alert("ERROR: " + e)
         }
     }
 
