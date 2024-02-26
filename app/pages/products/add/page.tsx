57072244be374381e4af2e8525ebcca5f71bf83b
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-02-26 13:18:31 +0100

    feat: create page to upload a new maj file

--- app/pages/products/add/page.tsx
+++ app/pages/products/add/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { TitleType } from '@models/Title'
 import { Title } from '@components/Title'
+import UploadForm from '@components/products//UploadMaj'
 
 export default function page() {
 
@@ -13,6 +14,20 @@ export default function page() {
   return (
     <div>
         <Title {...title} />
+
+        <div className='flex flex-col items-center add-product-card ml-auto mr-auto mt-10'>
+          <div className='flex flex-col items-center tutoriel'>
+            <p>
+              Pour mettre à jour vos produits, veuillez vous référer à ce tutoriel:
+            </p>
+            <button type="button">Tutoriel</button>
+          </div>
+
+          <div className='flex flex-col items-center upload'>
+            <p>Ensuite, uploader votre fichier ici:</p>
+            <UploadForm />
+          </div>
+        </div>
     </div>
   )
 }
