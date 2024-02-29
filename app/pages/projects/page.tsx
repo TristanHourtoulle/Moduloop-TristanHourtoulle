c6e6957785cc3b2308a42ab9bbc46ae18ee4ce4b
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-02-29 09:52:27 +0100

    feat: add design of projet card on /pages/projects

--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -9,37 +9,60 @@ import { GroupType } from '@models/Group'
 import { databaseToProjectModel, databaseToGroupModel } from '@utils/convert'
 import Link from 'next/link'
 import Image from 'next/image'
+import dateFormater from '@utils/dateFormater'
 
 export default function page() {
 
     const [projects, setProjects] = useState<ProjectType | null>(null);
+    const [group, setGroup] = useState<GroupType | null>(null);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
       const fetchData = async () => {
-          let res = await fetch('/api/project/list', {
+          // Get User Session
+          let res = await fetch('/api/session', {
               method: 'GET'
           });
-          const data = await res.json();
-          if (data.success) {
-              console.log("GET: ", data.data)
-              setProjects(databaseToProjectModel(data.data));
+          const session = await res.json();
+          if (!session.success) {
+              console.error('Failed to fetch session:', session.error);
+              alert(session.error);
           } else {
-              console.error('Failed to fetch projects:', data.error);
-          }
-
-          res = await fetch('/api/group/list', {
-              method: 'GET'
-          });
-          const groupData = await res.json();
-          if (groupData.success) {
-              console.log("GET: ", groupData.data)
-              setProjects(databaseToGroupModel(groupData.data));
-          } else {
-              console.error('Failed to fetch groups:', groupData.error);
+              console.log("Session: ", session.data)
+              await setUser(session.session.user);
+              // Get Projects
+              // Get Projects
+              res = await fetch(`/api/project/list?id=${encodeURIComponent(session.session.user.id)}`, {
+                method: 'GET'
+              });
+              const data = await res.json();
+              if (data.success) {
+                let projectsData = databaseToProjectModel(data.data);
+                for (let i = 0; i < projectsData.length; i++) {
+                    // Get Group By Id
+                    res = await fetch(`/api/group/list?id=${encodeURIComponent(projectsData[i].group)}`, {
+                        method: 'GET'
+                    });
+                    const groupData = await res.json();
+                    if (groupData.success) {
+                        const groupInfo = databaseToGroupModel(groupData.data);
+                        if (groupInfo.id) {
+                            projectsData[i].groupInfo = groupInfo;
+                        } else {
+                            console.log('Invalid group data:', groupInfo);
+                        }
+                    } else {
+                        console.error('Failed to fetch groups:', groupData.error);
+                    }
+                }
+                await setProjects(projectsData);
+              } else {
+                console.error('Failed to fetch projects:', data.error);
+              }
           }
       };
       fetchData();
-    }, []);
+  }, []);
 
     const title: TitleType = {
       title: "Vos projets",
@@ -50,7 +73,8 @@ export default function page() {
 
   return (
     <div>
-      <div className='flex items-center'>
+      {/* Header */}
+      <div className='flex items-center mb-10'>
         <Title {...title} />
         <Link href="/pages/projects/create" className='create-project-button'>
           <div className='flex gap-2'>
@@ -63,6 +87,42 @@ export default function page() {
             Créer un projet
           </div>
         </Link>
+      </div>
+      {/* Projects */}
+      <div className='flex items-center justify-center gap-4 mt-10'>
+        {projects ? projects.map((project, index) => {
+          const group = project.groupInfo;
+          return (
+            <div key={index} className='flex flex-col project-card gap-2 project-zoom'>
+                <p className='group'>{group ? group.name : 'Groupe non trouvé'}</p>
+                <p className='name'>{project.name}</p>
+                <p className='description'>{project.description}</p>
+                <div className='flex'>
+                  <p className='date mr-auto'>Dernières modifications:</p>
+                  <p className='date mr-5'>{dateFormater(project.updated_at).date} à {dateFormater(project.updated_at).time}</p>
+                </div>
+                <div className='line'></div>
+                <div className='flex justify-center items-center gap-10'>
+                  <Link href="#" className=''>
+                    <div className='flex items-center gap-2 delete-btn'>
+                      <Image
+                        src="/icons/trash-can.svg"
+                        alt="Supprimer le projet"
+                        width={30}
+                        height={30}
+                      />
+                    </div>
+                  </Link>
+                  <Link href="#">
+                    <div className='flex items-center gap-2 open-btn'>
+                      <p className=''>Ouvrir</p>
+                    </div>
+                  </Link>
+                </div>
+
+            </div>
+          )
+        }) : <p>Vous n'avez pas de projets pour le moment.</p>}
       </div>
     </div>
   )
