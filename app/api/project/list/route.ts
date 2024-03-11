28748bdac0133a5786e454359068ec31f8b17e5b
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-11 09:53:52 +0100

    feat: create api route to delete product in project

--- app/api/project/list/route.ts
+++ app/api/project/list/route.ts
@@ -3,6 +3,8 @@ import pool from '@/lib/database';
 import { NextRequest } from 'next/server';
 import { ProductType } from '@models/Product';
 import { User } from '@/models/User';
+import { deleteProductFromProject } from '@utils/projects';
+import { AddProductType } from '@models/AddProduct';
 
 // Fonction pour gérer les requêtes POST
 export async function POST(request: NextRequest) {
@@ -47,7 +49,34 @@ export async function PUT(request: NextRequest) {
 // Fonction pour gérer les requêtes DELETE
 export async function DELETE(request: NextRequest) {
     try {
-        return Response.json({success: true}, {status: 200});
+        const searchParams = request.nextUrl.searchParams;
+        const idProject = searchParams.get('id_project') || '';
+        const idProduct = searchParams.get('id_product') || '';
+
+        if (idProject === '' || idProduct === '') {
+            console.log('Parameters: ', idProject, idProduct);
+            throw new Error('Missing parameters for DELETE request.');
+        } else {
+            let res = await pool.query(`SELECT * FROM projects WHERE id = $1;`, [idProject]);
+            if (res.rowCount === 0) {
+                throw new Error('Project with this User not found.');
+            } else {
+                const project = res.rows[0];
+                const productsInProject = project.products;
+                const newProducts = deleteProductFromProject(Number(idProduct), productsInProject);
+
+                if (newProducts === productsInProject) {
+                    throw new Error('Product not found in project.');
+                } else {
+                    let res = await pool.query(`UPDATE projects SET products = $1 WHERE id = $2 RETURNING *;`, [JSON.stringify(newProducts), idProject]);
+                    if (!res) {
+                        throw new Error('Error while updating project.');
+                    }
+                    const data = res.rows[0];
+                    return Response.json({success: true, data: data}, {status: 200});
+                }
+            }
+        }
     } catch (error) {
         console.error('Erreur lors de l\'insertion de l\'utilisateur:', error);
         return Response.json({ success: false, error}, { status: 500 });
