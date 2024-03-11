fa82752b0622f9a10f3bd472f6da4610fc03896c
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-11 14:23:40 +0100

    feat: create new model for ranking impact

---
+++ app/models/MostImpact.tsx
@@ -0,0 +1,10 @@
+import { AddProductType } from "./AddProduct";
+
+export interface MostImpactType {
+    product: AddProductType;
+    totalRc: number;
+    totalErf: number;
+    totalAse: number;
+    totalEm: number;
+    totalImpact: number;
+}
