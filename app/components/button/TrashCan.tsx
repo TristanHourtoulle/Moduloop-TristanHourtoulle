adcf558fb5f6e19c1fbc3d1f2570b316c250c29a
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-12 12:39:07 +0100

    feat: better design for trash-can icon

--- app/components/button/TrashCan.tsx
+++ app/components/button/TrashCan.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 const TrashCan = () => {
   return (
-    <div className='delete-btn'>
+    <div className='delete-btn flex'>
         <Image
         src="/icons/trash-can.svg"
         alt="Supprimer le produit du projet"
-        width={20}
-        height={20}
+        width={25}
+        height={25}
         />
     </div>
   )
