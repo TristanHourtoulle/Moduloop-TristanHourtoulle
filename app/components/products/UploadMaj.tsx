0604b53d2b5348b33ea1016d3c79638ef1448f87
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-15 11:14:31 +0100

    feat: create process to handle file upload for new bdd to import

--- app/components/products/UploadMaj.tsx
+++ app/components/products/UploadMaj.tsx
@@ -2,23 +2,36 @@
 
 import { ProductType } from "@models/Product";
 import { useState } from "react";
-
+import Loader from "@components/Loader";
 
 async function addData(data: JSON) {
     try {
         const parseData = JSON.parse(data);
-        const addSection = parseData.Add;
-        addSection.forEach(async (element: any) => {
-            const res = await fetch('/api/product', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(element)
-            });
-            if (!res.ok) throw new Error(await res.text());
-            const updateData = await res.json();
+        const res = await fetch('/api/product', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(parseData)
         });
+        if (!res.ok) {
+            console.log("ERROR: ", await res.text());
+            throw new Error(await res.text())
+        };
+        const updateData = await res.json();
+        console.log("Data successfully added: ", updateData);
+        // const addSection = parseData.Add;
+        // addSection.forEach(async (element: any) => {
+        //     const res = await fetch('/api/product', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(element)
+        //     });
+        //     if (!res.ok) throw new Error(await res.text());
+        //     const updateData = await res.json();
+        // });
         return { success: true, message: "Data added" };
     } catch (e: any) {
         return { success: false, message: e.message };
@@ -27,8 +40,10 @@ async function addData(data: JSON) {
 
 export default function UploadForm() {
     const [file, setFile] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const onSubmit = async (e) => {
+        setIsLoading(true);
         e.preventDefault();
         if (!file) return
 
@@ -44,26 +59,34 @@ export default function UploadForm() {
             const updateData = await res.json();
 
             if (!updateData.success) {
+                setIsLoading(false);
                 return ("KO: " + updateData.message);
             }
-            addData(updateData.data);
+            await addData(updateData.data);
+            setIsLoading(false);
+            window.location.href = "/pages/products";
         } catch (e) {
+            setIsLoading(false);
             console.log("ERROR: ", e)
             alert("ERROR: " + e)
         }
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                type='file'
-                name='file'
-                onChange={(e) => setFile(e.target.files?.[0])}
-            />
-            <input
-                type="submit"
-                value="Upload"
-            />
-        </form>
+        isLoading
+            ? <Loader />
+            : (
+                <form onSubmit={onSubmit}>
+                    <input
+                        type='file'
+                        name='file'
+                        onChange={(e) => setFile(e.target.files?.[0])}
+                    />
+                    <input
+                        type="submit"
+                        value="Upload"
+                    />
+                </form>
+            )
     )
 }
