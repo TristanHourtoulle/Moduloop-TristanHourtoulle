358139eff72611e8f6cb2a6c26d6ce71814d83ac
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-19 14:26:50 +0100

    fix: display w and h of product page

--- app/pages/products/page.tsx
+++ app/pages/products/page.tsx
@@ -9,27 +9,28 @@ import { ProductType } from '@models/Product'
 import Card from '@components/products/Card'
 import Loader from '@components/Loader'
 
-export default function page() {
+export default function Page() {
 
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            const res = await fetch('/api/product/list', {
-                method: 'GET'
-            });
-            const data = await res.json();
-            if (data.success) {
-                setProducts(data.data);
-            } else {
-                console.error('Failed to fetch products:', data.error);
-            }
-            setIsLoading(false);
-        };
         fetchData();
     }, []);
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        const res = await fetch('/api/product/list', {
+            method: 'GET'
+        });
+        const data = await res.json();
+        if (data.success) {
+            setProducts(data.data);
+        } else {
+            console.error('Failed to fetch products:', data.error);
+        }
+        setIsLoading(false);
+    };
 
     const title: TitleType = {
         title: "Vos produits",
@@ -40,15 +41,15 @@ export default function page() {
         id_project: 0
     }
 
-  return (
-    <div>
-        <div className='flex items-center mb-10'>
-            <div className='mr-auto'>
-                <Title {...title} />
-            </div>
+    return (
+        <div>
+            <div className='flex items-center mb-10'>
+                <div className='mr-auto'>
+                    <Title {...title} />
+                </div>
 
-            <Link href="/pages/products/add">
-                <div className='flex items-center justify-center gap-2 mr-5 handle-product-button'>
+                <Link href="/pages/products/add">
+                    <div className='flex items-center justify-center gap-2 mr-5 handle-product-button'>
                         <Image
                             src="/icons/reglages.svg"
                             alt="Add Product"
@@ -57,28 +58,27 @@ export default function page() {
                             className='object-contain handle-product-image'
                         />
                         <p className='handle-product-text'>Gérer</p>
-                </div>
-            </Link>
-        </div>
-
-        {isLoading ? (
-            <Loader />)
-        :
-            <div className='ml-[3%] scroll-view overflow-y-hidden'>
-                <div  className="products-section my-[2%] mx-[5%]" style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', gap: '25px', justifyContent: 'space-between' }}>
-                    {products ? (
-                        products.map((product, index) => (
-                            <Card key={index} {...product}/>
-                        ))
-                    ) : (
-                        <div className='flex items-center justify-center'>
-                            <p className='no-product-text'>Oh, c'est bien vide par ici...</p>
-                        </div>
-                    )}
-                </div>
+                    </div>
+                </Link>
             </div>
-        }
-    </div>
-  )
-}
 
+            {isLoading ? (
+                <Loader />)
+            :
+                <div className='ml-[3%] scroll-view overflow-y-hidden'>
+                    <div className="products-section my-[2%] mx-[5%]" style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', gap: '25px', justifyContent: 'space-between' }}>
+                        {products ? (
+                            products.map((product, index) => (
+                                <Card key={index} {...product} onDeleteSuccess={fetchData} />
+                            ))
+                        ) : (
+                            <div className='flex items-center justify-center'>
+                                <p className='no-product-text'>Oh, c'est bien vide par ici...</p>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
