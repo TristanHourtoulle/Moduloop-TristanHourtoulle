63913642c403deea0bb6cb54dd6cc2fb222509fc
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-19 14:31:08 +0100

    fix: sort all users by last activities

--- app/api/user/list/route.ts
+++ app/api/user/list/route.ts
@@ -1,9 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import pool from "../../../lib/database";
 
 export async function GET(request: NextRequest) {
   try {
-    const result = await pool.query("SELECT * FROM users;");
+    const result = await pool.query(
+      'SELECT * FROM users ORDER BY "updatedAt" DESC;'
+    );
     if (result.rowCount === 0) {
       throw new Error("Aucun utilisateur trouvé dans la base de données");
     }
