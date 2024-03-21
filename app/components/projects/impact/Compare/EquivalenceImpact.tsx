d17bae06d7fec9b0e79d6468a663150b0a2e40f3
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-21 16:51:51 +0100

    feat: add Equivalence template with conversion

--- app/components/projects/impact/Compare/EquivalenceImpact.tsx
+++ app/components/projects/impact/Compare/EquivalenceImpact.tsx
@@ -1,18 +1,47 @@
 import { ProjectType } from "@models/Project";
+import { useEffect, useState } from "react";
 import { CardEquivalenceImpact } from "../Card/CardEquivalenceImpact";
 
 export type EquivalenceImpactProps = {
   project: ProjectType;
   worstProject: ProjectType;
-  planeValue: number;
-  personValue: number;
-  kmValue: number;
+  title1: string;
+  title2: string;
+  title3: string;
+  value1: number;
+  value2: number;
+  value3: number;
+  unit1: string;
+  unit2: string;
+  unit3: string;
 };
 
 export const EquivalenceImpact = (props: EquivalenceImpactProps) => {
-  const { project, worstProject, planeValue, personValue, kmValue } = props;
+  const {
+    project,
+    worstProject,
+    value1,
+    value2,
+    value3,
+    title1,
+    title2,
+    title3,
+    unit1,
+    unit2,
+    unit3,
+  } = props;
 
-  console.log("EquivalenceImpact In Section: ", planeValue);
+  const [image1, setImage1] = useState<string>("/icons/avion.svg");
+  const [image2, setImage2] = useState<string>("/icons/personnes.svg");
+  const [image3, setImage3] = useState<string>("/icons/suv.svg");
+
+  useEffect(() => {
+    if (title1 === "pétrol brut") {
+      setImage1("/icons/pétrol.svg");
+      setImage2("/icons/lampadaire.svg");
+      setImage3("/icons/maison.svg");
+    }
+  });
 
   return (
     <div className="w-full min-h-60 flex flex-col gap-5 impact-section-card">
@@ -20,28 +49,28 @@ export const EquivalenceImpact = (props: EquivalenceImpactProps) => {
       <div className="flex flex-col sm:flex-row items-center gap-5">
         <CardEquivalenceImpact
           project={project}
-          title="PARIS - NICE"
-          image="/icons/avion.svg"
-          value={planeValue}
-          unit="Aller - Retour"
+          title={title1}
+          image={image1}
+          value={value1}
+          unit={unit1}
           worstProject={worstProject}
         />
         <p className="text-lg font-bold">ou</p>
         <CardEquivalenceImpact
           project={project}
-          title="émission journalière"
-          image="/icons/personnes.svg"
-          value={personValue}
-          unit="Français"
+          title={title2}
+          image={image2}
+          value={value2}
+          unit={unit2}
           worstProject={worstProject}
         />
         <p className="text-lg font-bold">ou</p>
         <CardEquivalenceImpact
           project={project}
-          title="kms parcourus en suv"
-          image="/icons/suv.svg"
-          value={kmValue}
-          unit="Kms"
+          title={title3}
+          image={image3}
+          value={value3}
+          unit={unit3}
           worstProject={worstProject}
         />
       </div>
