997ad48e620efaab826b4bc7a4f8d754a31c818b
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-21 16:53:57 +0100

    feat: update ImpactProject Card template for matching with rc, erf, ase, em

--- app/components/projects/impact/Card/CardImpactProject.tsx
+++ app/components/projects/impact/Card/CardImpactProject.tsx
@@ -1,21 +1,47 @@
 import { ProjectType } from "@models/Project";
+import {
+  getASEimpact,
+  getCO2impact,
+  getEMimpact,
+  getERFimpact,
+} from "@utils/getImpact";
 import Image from "next/image";
-import { getCO2impact } from "@utils/getImpact";
+import { useEffect, useState } from "react";
 
 export type CardImpactProjectProps = {
   project: ProjectType;
   type: string;
+  impact: string;
 };
 
 export const CardImpactProject = (props: CardImpactProjectProps) => {
-  const { project, type } = props;
-  const result = getCO2impact(project);
-  let image = "/icons/feuille.svg";
-  console.log("project: ", project);
+  const { project, type, impact } = props;
+  const [unit, setUnit] = useState("kgCO2e");
+  const [image, setImage] = useState("/icons/feuille.svg");
+  const [result, setResult] = useState(0);
 
-  if (type !== "better") {
-    image = "/icons/rc.svg";
-  }
+  useEffect(() => {
+    if (impact === "rc") {
+      setResult(Number(getCO2impact(project)) || 0);
+      setUnit("kgCO2e");
+    } else if (impact === "erf") {
+      setResult(Number(getERFimpact(project)) || 0);
+      setUnit("MJ");
+    } else if (impact === "ase") {
+      setResult(Number(getASEimpact(project)) || 0);
+      setUnit("mol H+");
+    } else if (impact === "em") {
+      setResult(Number(getEMimpact(project)) || 0);
+      setUnit("kg P eq.");
+    }
+
+    if (type !== "better") {
+      if (impact === "rc") setImage("/icons/rc.svg");
+      if (impact === "erf") setImage("/icons/erf.svg");
+      if (impact === "ase") setImage("/icons/ase.svg");
+      if (impact === "em") setImage("/icons/em.svg");
+    }
+  });
 
   return (
     <div className="px-8 py-4 bg-white flex flex-col gap-2 rounded-[10px] drop-shadow-lg w-full">
@@ -32,7 +58,7 @@ export const CardImpactProject = (props: CardImpactProjectProps) => {
         />
         <div className="flex items-end gap-3">
           <p className="text-black font-bold text-5xl">{result}</p>
-          <p className="text-black font-regular text-xl">kgCO2e</p>
+          <p className="text-black font-regular text-xl">{unit}</p>
         </div>
       </div>
     </div>
