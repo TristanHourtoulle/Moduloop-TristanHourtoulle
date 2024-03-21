109d18e3c938f6b4d9372137a590d02ea2ca730c
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-21 16:54:44 +0100

    feat: display all Impact infos (rc, erf, ase) but not em because value are too small so it's useless

--- app/components/projects/ImpactSection.tsx
+++ app/components/projects/ImpactSection.tsx
@@ -1,6 +1,11 @@
 import { AddProductType } from "@models/AddProduct";
 import { ProjectType } from "@models/Project";
-import { getCO2impact } from "@utils/getImpact";
+import {
+  getASEimpact,
+  getCO2impact,
+  getEMimpact,
+  getERFimpact,
+} from "@utils/getImpact";
 import { useEffect, useState } from "react";
 import { CompareImpact } from "./impact/Compare/CompareImpact";
 import { EquivalenceImpact } from "./impact/Compare/EquivalenceImpact";
@@ -17,12 +22,18 @@ const ImpactSection = (props: {
   const [isCompare, setIsCompare] = useState(false);
   const [session, setSession] = useState(null);
   const [compareWith, setCompareWith] = useState<ProjectType | null>(null);
-  const [percentage, setPercentage] = useState<number>(0.0);
+  const [rcPercentage, setRcPercentage] = useState<number>(0.0);
+  const [erfPercentage, setErfPercentage] = useState<number>(0.0);
+  const [asePercentage, setAsePercentage] = useState<number>(0.0);
+  const [emPercentage, setEmPercentage] = useState<number>(0.0);
   const [betterProject, setBetterProject] = useState<ProjectType>(project);
   const [worstProject, setWorstProject] = useState<ProjectType>(project);
   const [planeEquivalent, setPlaneEquivalent] = useState<number>(0);
   const [personEquivalent, setPersonEquivalent] = useState<number>(0);
   const [kmEquivalent, setKmEquivalent] = useState<number>(0);
+  const [petrolEquivalent, setPetrolEquivalent] = useState<number>(0);
+  const [lampEquivalent, setLampEquivalent] = useState<number>(0);
+  const [houseEquivalent, setHouseEquivalent] = useState<number>(0);
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -54,7 +65,31 @@ const ImpactSection = (props: {
       console.log("isCompare value changed: ", compareWith);
       const value1: number = Number(getCO2impact(project));
       const value2: number = Number(getCO2impact(compareWith));
+      const erfValue1: number = Number(getERFimpact(project));
+      const erfValue2: number = Number(getERFimpact(compareWith));
+      const aseValue1: number = Number(getASEimpact(project));
+      const aseValue2: number = Number(getASEimpact(compareWith));
+      const emValue1: number = Number(getEMimpact(project));
+      const emValue2: number = Number(getEMimpact(compareWith));
       let percentage;
+      if (erfValue1 > erfValue2) {
+        percentage = ((erfValue1 - erfValue2) / erfValue1) * 100;
+      } else {
+        percentage = ((erfValue2 - erfValue1) / erfValue2) * 100;
+      }
+      setErfPercentage(Number(percentage.toFixed(0)));
+      if (aseValue1 > aseValue2) {
+        percentage = ((aseValue1 - aseValue2) / aseValue1) * 100;
+      } else {
+        percentage = ((aseValue2 - aseValue1) / aseValue2) * 100;
+      }
+      setAsePercentage(Number(percentage.toFixed(0)));
+      if (emValue1 > emValue2) {
+        percentage = ((emValue1 - emValue2) / emValue1) * 100;
+      } else {
+        percentage = ((emValue2 - emValue1) / emValue2) * 100;
+      }
+      setEmPercentage(Number(percentage.toFixed(0)));
 
       if (value1 > value2) {
         percentage = ((value1 - value2) / value1) * 100;
@@ -71,6 +106,16 @@ const ImpactSection = (props: {
         // Km equivalent
         result = delta / 0.17;
         setKmEquivalent(Number(result.toFixed(0)));
+        // Petrol equivalent
+        delta = erfValue1 - erfValue2;
+        result = delta / 5861;
+        setPetrolEquivalent(Number(result.toFixed(0)));
+        // Lamp equivalent
+        result = delta / 648;
+        setLampEquivalent(Number(result.toFixed(0)));
+        // House equivalent
+        result = delta / 147.96;
+        setHouseEquivalent(Number(result.toFixed(0)));
       } else {
         percentage = ((value2 - value1) / value2) * 100;
         setBetterProject(project);
@@ -86,9 +131,19 @@ const ImpactSection = (props: {
         // Km equivalent
         result = delta / 0.17;
         setKmEquivalent(Number(result.toFixed(0)));
+        // Petrol equivalent
+        delta = erfValue2 - erfValue1;
+        result = delta / 5861;
+        setPetrolEquivalent(Number(result.toFixed(0)));
+        // Lamp equivalent
+        result = delta / 648;
+        setLampEquivalent(Number(result.toFixed(0)));
+        // House equivalent
+        result = delta / 147.96;
+        setHouseEquivalent(Number(result.toFixed(0)));
       }
 
-      setPercentage(Number(percentage.toFixed(0)));
+      setRcPercentage(Number(percentage.toFixed(0)));
     } else {
       console.log("Don't compare with any project");
     }
@@ -96,19 +151,6 @@ const ImpactSection = (props: {
 
   return (
     <div className="impact flex flex-col items-start gap-5 my-[2%] mx-[5%]">
-      {/* isCompare
-      <div
-        className="btn-compare cursor-pointer transition-all hover:opacity-80"
-        onClick={() => {
-          setIsCompare(!isCompare);
-        }}
-      >
-        <GitCompareArrows size={25} />
-        <p className="text-lg font-bold">
-          {isCompare ? "Arrêter de comparer" : "Comparer à un autre projet"}
-        </p>
-      </div> */}
-
       <div className="w-full flex flex-col gap-6">
         {/* Select project for compare */}
         <div className="flex items-center gap-5">
@@ -148,22 +190,72 @@ const ImpactSection = (props: {
           <CompareImpact
             project_one={project}
             project_two={compareWith}
-            percentage={percentage}
+            percentage={rcPercentage}
+            type="Réchauffement climatique"
           />
         )}
         {isCompare && compareWith && (
           <EquivalenceImpact
             project={betterProject}
             worstProject={worstProject}
-            planeValue={planeEquivalent}
-            personValue={personEquivalent}
-            kmValue={kmEquivalent}
+            value1={planeEquivalent}
+            value2={personEquivalent}
+            value3={kmEquivalent}
+            title1="paris - nice"
+            title2="émission journalière"
+            title3="kms parcourus"
+            unit1="Aller - Retour"
+            unit2="Français"
+            unit3="Kms"
           />
         )}
-        {/* Impact Globale Project */}
-        <ImpactGlobalProject project_one={project} />
-        {/* Most Impact List */}
-        <MostImpact project={project} />
+        {isCompare && compareWith && (
+          <CompareImpact
+            project_one={project}
+            project_two={compareWith}
+            percentage={erfPercentage}
+            type="Epuisement des ressources fossiles"
+          />
+        )}
+        {isCompare && compareWith && (
+          <EquivalenceImpact
+            project={betterProject}
+            worstProject={worstProject}
+            value1={petrolEquivalent}
+            value2={lampEquivalent}
+            value3={houseEquivalent}
+            title1="pétrol brut"
+            title2="éclairage d'un lampadaire"
+            title3="chauffage"
+            unit1="Barils"
+            unit2="Années"
+            unit3="Jours"
+          />
+        )}
+
+        {isCompare && compareWith && (
+          <CompareImpact
+            project_one={project}
+            project_two={compareWith}
+            percentage={asePercentage}
+            type="Acidification des sols et eaux"
+          />
+        )}
+
+        {isCompare && compareWith && (
+          <div className="hidden">
+            <CompareImpact
+              project_one={project}
+              project_two={compareWith}
+              percentage={emPercentage}
+              type="Eutrophisation marine"
+            />
+          </div>
+        )}
+
+        {!isCompare && <ImpactGlobalProject project_one={project} />}
+
+        {!isCompare && <MostImpact project={project} />}
       </div>
     </div>
   );
