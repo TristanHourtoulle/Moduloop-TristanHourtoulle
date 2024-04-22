5b9a7d3fc5be070493356bdac980ab39c048856e
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-22 09:41:53 +0200

    fix: update display of table in this page

--- app/pages/users/page.tsx
+++ app/pages/users/page.tsx
@@ -79,64 +79,59 @@ export default function Page() {
       {!isLoading && (
         <div className="h-full">
           {/* Header */}
-          <Title {...title} />
+          {title && <Title {...title} />}
 
           {/* Content */}
           <div className="py-8 flex items-center justify-center">
             {users && users.length > 0 && (
               <div className="w-auto">
-                {users.map((user, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center justify-center overflow-x-auto shadow-md sm:rounded-lg mb-4"
-                  >
-                    <table className="w-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                          <th scope="col" className="px-6 py-3">
-                            Nom
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Prénom
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Email
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Rôle
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Dernières activités
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Action
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <tr>
-                          <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {user.name}
-                          </td>
-                          <td className="px-6 py-4">{user.firstName}</td>
-                          <td className="px-6 py-4">{user.email}</td>
-                          <td className="px-6 py-4">{user.role}</td>
-                          <td className="px-6 py-4">
-                            {formatDate(user.updatedAt)}
-                          </td>
-                          <td className="px-6 py-4">
-                            <Link
-                              href={`/pages/users/${user.id}`}
-                              className="transition-all ease-in-out delay-50 text-white opacity-50 hover:opacity-100 flex items-center gap-2"
-                            >
-                              <Pencil />
-                            </Link>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                ))}
+                <table className="w-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">
+                        Nom
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Prénom
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Email
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Rôle
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Date de création
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    {users.map((user, index) => (
+                      <tr key={index}>
+                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {user.name}
+                        </td>
+                        <td className="px-6 py-4">{user.firstName}</td>
+                        <td className="px-6 py-4">{user.email}</td>
+                        <td className="px-6 py-4">{user.role}</td>
+                        <td className="px-6 py-4">
+                          {formatDate(user.updatedAt)}
+                        </td>
+                        <td className="px-6 py-4">
+                          <Link
+                            href={`/pages/users/${user.id}`}
+                            className="transition-all ease-in-out delay-50 text-white opacity-50 hover:opacity-100 flex items-center gap-2"
+                          >
+                            <Pencil />
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             )}
           </div>
