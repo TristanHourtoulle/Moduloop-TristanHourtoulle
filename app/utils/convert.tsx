b8852ab4c1d0751568834a1cc02c6e8d1ff7e04f
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-22 09:29:36 +0200

    feat: add some converter function from DB to models/

--- app/utils/convert.tsx
+++ app/utils/convert.tsx
@@ -4,102 +4,102 @@ import { ProductType } from "@models/Product";
 import { AddProductType } from "@models/AddProduct";
 
 export function databaseToSingleProductModel(data: JSON) {
-    let product: ProductType = {
-        id: data.id,
-        name: data.name,
-        image: data.image,
-        unit: data.unit,
-        base: data.base,
-        new: data.new,
-        reuse: data.reuse,
-    }
+  let product: ProductType = {
+    id: data.id,
+    name: data.name,
+    image: data.image,
+    unit: data.unit,
+    base: data.base,
+    new: data.new,
+    reuse: data.reuse,
+  };
 
-    return product;
+  return product;
 }
 
 export function databaseToGroupModel(data: JSON) {
-    let groups: GroupType[] = [];
+  let groups: GroupType[] = [];
 
-    const dataArray = Array.from(data);
-    for (let i = 0; i < dataArray.length; i++) {
-        let group: GroupType = {
-            id: data[i].id,
-            name: data[i].name,
-            description: data[i].description,
-            image: data[i].image,
-            budget: data[i].budget,
-            user_id: data[i].user_id
-        }
-        groups.push(group);
-    }
+  const dataArray = Array.from(data);
+  for (let i = 0; i < dataArray.length; i++) {
+    let group: GroupType = {
+      id: data[i].id,
+      name: data[i].name,
+      description: data[i].description,
+      image: data[i].image,
+      budget: data[i].budget,
+      user_id: data[i].user_id,
+    };
+    groups.push(group);
+  }
 
-    return groups[0];
+  return groups[0];
 }
 
 export function databaseToSeveralGroupModel(data: JSON) {
-    let groups: GroupType[] = [];
+  let groups: GroupType[] = [];
 
-    const dataArray = Array.from(data);
-    for (let i = 0; i < dataArray.length; i++) {
-        let group: GroupType = {
-            id: data[i].id,
-            name: data[i].name,
-            description: data[i].description,
-            image: data[i].image,
-            budget: data[i].budget,
-            user_id: data[i].user_id
-        }
-        groups.push(group);
-    }
+  const dataArray = Array.from(data);
+  for (let i = 0; i < dataArray.length; i++) {
+    let group: GroupType = {
+      id: data[i].id,
+      name: data[i].name,
+      description: data[i].description,
+      image: data[i].image,
+      budget: data[i].budget,
+      user_id: data[i].user_id,
+    };
+    groups.push(group);
+  }
 
-    return groups;
+  return groups;
 }
 
 export function databaseToProjectModel(data: JSON) {
-    let projects: ProjectType[] = [];
+  let projects: ProjectType[] = [];
 
-    const dataArray = Array.from(data);
-    for (let i = 0; i < dataArray.length; i++) {
-        let project: ProjectType = {
-            id: data[i].id,
-            name: data[i].name,
-            description: data[i].description,
-            image: data[i].image,
-            budget: data[i].budget,
-            products: data[i].products,
-            company: data[i].company,
-            location: data[i].location,
-            area: data[i].area,
-            user_id: data[i].user_id,
-            group: data[i].group_id,
-            created_at: data[i].created_at,
-            updated_at: data[i].updated_at
-        }
-        projects.push(project);
-    }
+  const dataArray = Array.from(data);
+  for (let i = 0; i < dataArray.length; i++) {
+    let project: ProjectType = {
+      id: data[i].id,
+      name: data[i].name,
+      description: data[i].description,
+      image: data[i].image,
+      budget: data[i].budget,
+      products: data[i].products,
+      company: data[i].company,
+      location: data[i].location,
+      area: data[i].area,
+      user_id: data[i].user_id,
+      group: data[i].group_id,
+      created_at: data[i].created_at,
+      updated_at: data[i].updated_at,
+    };
+    projects.push(project);
+  }
 
-    return projects;
+  return projects;
 }
 
 export function databaseToSingleProjectModel(data: JSON) {
-    let products: AddProductType[] = [];
+  let products: AddProductType[] = [];
 
-    let project: ProjectType = {
-        id: data.id,
-        name: data.name,
-        description: data.description,
-        image: data.image,
-        budget: data.budget,
-        products: data.products,
-        company: data.company,
-        location: data.location,
-        area: data.area,
-        user_id: data.user_id,
-        group: data.group_id,
-        created_at: data.created_at,
-        updated_at: data.updated_at,
-        groupInfo: null
-    }
+  let project: ProjectType = {
+    id: data.id,
+    name: data.name,
+    description: data.description,
+    image: data.image,
+    budget: data.budget,
+    products: data.products,
+    company: data.company,
+    location: data.location,
+    area: data.area,
+    user_id: data.user_id,
+    group: data.group_id,
+    created_at: data.created_at,
+    updated_at: data.updated_at,
+    groupInfo: null,
+  };
 
-    return project;
+  return project;
 }
