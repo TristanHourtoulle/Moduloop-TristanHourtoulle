838306c76530054010ebdc9579a05c3bbc96730d
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-22 09:37:46 +0200

    fix: fix crash on product page

--- app/pages/products/[id]/page.tsx
+++ app/pages/products/[id]/page.tsx
@@ -1,25 +1,19 @@
-"use client"
+"use client";
 
-import { ProductType } from "@models/Product"
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { TitleType } from "@models/Title";
-import { Title } from "@components/Title";
-import Image from "next/image";
-import InfoTable from "@components/products/infoTable";
-import ImpactTable from "@components/products/impactTable";
 import Loader from "@components/Loader";
+import { Title } from "@components/Title";
+import ImpactTable from "@components/products/impactTable";
+import InfoTable from "@components/products/infoTable";
+import { ProductType } from "@models/Product";
+import { TitleType } from "@models/Title";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Page({
-    params: { id },
-  }: {
-    params: { id: string }
-  }) {
+export default function Page({ params: { id } }: { params: { id: string } }) {
   const [loading, setLoading] = useState<boolean>(true);
   const [myProduct, setMyProduct] = useState<ProductType | null>(null);
   const [title, setTitle] = useState<TitleType | null>(null);
-  const [view, setView] = useState("new") // "new" - "reuse"
+  const [view, setView] = useState("new"); // "new" - "reuse"
   const [file, setFile] = useState(null);
   const [isLoading, setIsLoading] = useState(true); // Set initial value to true
 
@@ -38,9 +32,20 @@ export default function Page({
       });
 
       if (response.ok) {
-        window.location.reload();
+        let res = await fetch(`/api/product?id=${encodeURIComponent(id)}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (res.ok) {
+          const product = await res.json();
+          setMyProduct(product.product);
+        } else {
+          // Handle error
+        }
       } else {
-        alert("Failed to upload image.")
+        alert("Failed to upload image.");
       }
     } catch (error) {
       alert(error);
@@ -53,30 +58,33 @@ export default function Page({
     const fetchProduct = async () => {
       setIsLoading(true); // Set isLoading to true before fetch operation
       try {
-        const response = await fetch(`/api/product?id=${encodeURIComponent(id)}`, {
+        const response = await fetch(
+          `/api/product?id=${encodeURIComponent(id)}`,
+          {
             method: "GET",
             headers: {
-                "Content-Type": "application/json"
-            }
-        });
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (response.ok) {
-            const product = await response.json();
-            setMyProduct(product.product);
-            if (product.product) {
-              const title: TitleType = {
-                title: "Votre produit: ",
-                image: "/icons/close.svg",
-                number: product.product.name,
-                back: "/pages/products",
-                canChange: false
-              }
-              setTitle(title);
-            }
+          const product = await response.json();
+          setMyProduct(product.product);
+          if (product.product) {
+            const title: TitleType = {
+              title: "Votre produit: ",
+              image: "/icons/close.svg",
+              number: product.product.name,
+              back: "/pages/products",
+              canChange: false,
+            };
+            setTitle(title);
+          }
         } else {
-            // Handle error
+          // Handle error
         }
       } catch (error) {
-        alert(error)
+        alert(error);
       } finally {
         setIsLoading(false); // Set isLoading to false after fetch operation
       }
@@ -88,71 +96,91 @@ export default function Page({
   return (
     <div className="">
       {!isLoading ? (
-        myProduct ?
-        <div className="">
+        myProduct ? (
+          <div className="">
             <div className="flex items-center mb-10">
-                <div className='mr-auto'>
-                    <Title {...title} />
+              <div className="mr-auto">
+                <Title {...title} />
+              </div>
+              <button type="button">
+                <div className="flex items-center gap-2 download-button ">
+                  <Image
+                    src="/icons/telecharger.svg"
+                    alt="download"
+                    height={30}
+                    width={30}
+                  />
+                  <p>Télécharger</p>
                 </div>
-                <button type="button">
-                    <div className="flex items-center gap-2 download-button ">
-                        <Image
-                            src="/icons/telecharger.svg"
-                            alt="download"
-                            height={30}
-                            width={30}
-                            />
-                        <p>Télécharger</p>
-                    </div>
-                </button>
+              </button>
             </div>
 
             <div className="flex items-center">
-                <div className="flex flex-col items-center mr-auto">
-                    {myProduct.image != '' && myProduct.image ? <Image
-                        src={myProduct.image ?? ''}
-                        alt={myProduct.name ?? ''}
-                        width={450}
-                        height={450}
-                        className='object-contain'
-                    /> : <Image
+              <div className="flex flex-col items-center mr-auto">
+                {myProduct.image != "" && myProduct.image ? (
+                  <Image
+                    src={myProduct.image ?? ""}
+                    alt={myProduct.name ?? ""}
+                    width={450}
+                    height={450}
+                    className="object-contain"
+                  />
+                ) : (
+                  <Image
                     src="/icons/no-image.png"
                     alt=""
                     width={450}
                     height={450}
-                    className='object-contain'
-                />}
+                    className="object-contain"
+                  />
+                )}
                 <form onSubmit={handleSubmit}>
                   <input
-                    type='file'
-                    name='file'
+                    type="file"
+                    name="file"
                     onChange={(e) => setFile(e.target.files?.[0])}
                   />
-                  <input
-                    type="hidden"
-                    name="productId"
-                    value={id}
-                  />
+                  <input type="hidden" name="productId" value={id} />
                   <input
                     type="submit"
                     value="Changer"
                     className="open-button"
                   />
                 </form>
+              </div>
+              <div className="flex flex-col items-center gap-10 mr-20">
+                <InfoTable {...myProduct} />
+                <div className="flex items-center gap-10">
+                  <button
+                    className={
+                      view === "new"
+                        ? "inter-nav-bar"
+                        : "inter-nav-bar-selected"
+                    }
+                    onClick={() => setView("new")}
+                  >
+                    Neuf
+                  </button>
+                  <button
+                    className={
+                      view === "reuse"
+                        ? "inter-nav-bar"
+                        : "inter-nav-bar-selected"
+                    }
+                    onClick={() => setView("reuse")}
+                  >
+                    Reuse
+                  </button>
                 </div>
-                <div className="flex flex-col items-center gap-10 mr-20">
-                    <InfoTable {...myProduct} />
-                    <div className="flex items-center gap-10">
-                        <button className={view === 'new' ? "inter-nav-bar" : "inter-nav-bar-selected" } onClick={() => setView("new")}>Neuf</button>
-                        <button className={view === 'reuse' ? "inter-nav-bar" : "inter-nav-bar-selected" } onClick={() => setView("reuse")}>Reuse</button>
-                    </div>
-                    <ImpactTable product={myProduct} type={view} />
-                </div>
+                <ImpactTable product={myProduct} type={view} />
+              </div>
             </div>
-
-        </div> : <div>
+          </div>
+        ) : (
+          <div>
             <p>Produit introuvable</p>
-        </div>
+          </div>
+        )
       ) : (
         <div>
           <Loader />
