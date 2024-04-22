9773a4035519f0dbeabfd20ceb0b835c82c67bfe
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-22 09:39:20 +0200

    feat: redesign the project pagr

--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -6,18 +6,32 @@ import { getSession } from "@lib/session";
 import { GroupType } from "@models/Group";
 import { ProjectType } from "@models/Project";
 import { TitleType } from "@models/Title";
-import { databaseToGroupModel, databaseToProjectModel } from "@utils/convert";
+import {
+  databaseToGroupModel,
+  databaseToProjectModel,
+  databaseToSeveralGroupModel,
+} from "@utils/convert";
 import dateFormater from "@utils/dateFormater";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function page() {
-  const [projects, setProjects] = useState<ProjectType | null>(null);
-  const [group, setGroup] = useState<GroupType | null>(null);
+  const [projects, setProjects] = useState<ProjectType[] | null>(null);
+  const [backupProjects, setBackupProjects] = useState<ProjectType[] | null>(
+    null
+  );
+  const [groups, setGroups] = useState<GroupType[]>([]);
+  const [selectedGroup, setSelectedGroup] = useState(-1);
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [userSession, getUserSession] = useState(null);
+
+  useEffect(() => {
+    if (backupProjects !== null) {
+      setProjects(filterProjects());
+    }
+  }, [selectedGroup, backupProjects]);
 
   useEffect(() => {
     const fetchSession = async () => {
@@ -30,6 +44,21 @@ export default function page() {
       }
       window.location.href = "/";
     };
+
+    const fetchGroup = async (id_user: string) => {
+      let res = await fetch(`/api/group/list?id=${id_user}`, {
+        method: "GET",
+      });
+      const data = await res.json();
+      if (data.success) {
+        let groupData = databaseToSeveralGroupModel(data.data);
+        await setGroups(groupData);
+        console.log("Group Fetch: ", groupData);
+      } else {
+        console.error("Failed to fetch groups:", data.error);
+      }
+    };
+
     const fetchData = async () => {
       setIsLoading(true);
       // Get User Session
@@ -42,7 +71,7 @@ export default function page() {
         alert(session.error);
       } else {
         await setUser(session.session.user);
-        // Get Projects
+        await fetchGroup(session.session.user.id);
         // Get Projects
         res = await fetch(
           `/api/project/list?id=${encodeURIComponent(session.session.user.id)}`,
@@ -64,16 +93,26 @@ export default function page() {
             const groupData = await res.json();
             if (groupData.success) {
               const groupInfo = databaseToGroupModel(groupData.data);
-              if (groupInfo.id) {
+              if (groupInfo && groupInfo.id) {
                 projectsData[i].groupInfo = groupInfo;
               } else {
-                console.log("Invalid group data:", groupInfo);
+                const tempGroupInfo: GroupType = {
+                  id: -1,
+                  name: "Aucun Groupe",
+                  description: "",
+                  budget: "0",
+                  user_id: session.session.user.id,
+                  image: "",
+                };
+                projectsData[i].groupInfo = tempGroupInfo;
               }
             } else {
               console.error("Failed to fetch groups:", groupData.error);
             }
           }
-          await setProjects(projectsData);
+          setProjects(projectsData);
+          setBackupProjects(projectsData); // Sauvegarder les projets initiaux
+          console.log("Projects Fetch: ", projectsData);
         } else {
           console.error("Failed to fetch projects:", data.error);
         }
@@ -83,6 +122,21 @@ export default function page() {
 
     fetchSession();
   }, []);
+
+  const filterProjects = () => {
+    if (backupProjects === null) return [];
+    if (selectedGroup === -1) {
+      return backupProjects;
+    } else {
+      const tempProjects: ProjectType[] = [];
+      for (let i = 0; i < backupProjects.length; i++) {
+        if (backupProjects[i].groupInfo.id === selectedGroup) {
+          tempProjects.push(backupProjects[i]);
+        }
+      }
+      return tempProjects;
+    }
+  };
 
   const title: TitleType = {
     title: "Vos projets",
@@ -137,7 +191,7 @@ export default function page() {
       {/* Header et Projects */}
       {!isLoading && (
         <>
-          <div className="flex items-center mb-10">
+          <div className="flex items-center mb-5">
             <Title {...title} />
             <Link
               href="/pages/projects/create"
@@ -155,7 +209,35 @@ export default function page() {
             </Link>
           </div>
 
-          <div className="flex items-center justify-center gap-4 mt-10 flex-wrap ml-10 projects-container">
+          <div className="ml-[6%] flex gap-5 items-center w-[40%]">
+            <label
+              hidden
+              htmlFor="selectedGroup"
+              className="text-lg font-medium"
+            >
+              Filtrer par groupe
+            </label>
+            <select
+              onChange={(event) => {
+                console.log("Selected Group: ", event.target.value);
+                setSelectedGroup(parseInt(event.target.value));
+              }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[65%] p-2.5 "
+            >
+              <option className="" value={-1}>
+                Tous les groupes
+              </option>
+
+              {groups.length > 0 &&
+                groups.map((group, index) => (
+                  <option key={index} value={group.id || -10}>
+                    {group.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div className="flex items-center justify-center gap-4 mt-[2%] flex-wrap ml-10 projects-container">
             {projects ? (
               projects.map((project, index) => {
                 const group = project.groupInfo;
@@ -165,9 +247,15 @@ export default function page() {
                     key={index}
                     className="flex flex-col project-card gap-2 project-zoom"
                   >
-                    <p className="group text-in-single-line">
-                      {group ? group.name : "Aucun Groupe"}
-                    </p>
+                    {group && group.name !== "Aucun Groupe" ? (
+                      <p className="text-in-single-line text-lg opacity-80">
+                        {group.name}
+                      </p>
+                    ) : (
+                      <p className="text-in-single-line text-lg opacity-80">
+                        Aucun Groupe
+                      </p>
+                    )}
                     <p className="name text-in-single-line">{project.name}</p>
                     <p className="description text-in-single-line">
                       {project.description
