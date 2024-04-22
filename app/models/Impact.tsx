8bc90b1d660fd6f0cbfabb7d2dee2d4a8587c438
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-22 09:36:26 +0200

    feat: update the models for Impact

--- app/models/Impact.tsx
+++ app/models/Impact.tsx
@@ -1,26 +1,38 @@
+import { AddProductType } from "./AddProduct";
+
 export interface GlobalImpact {
-    rc: {
-        manufacturing: 0,
-        installation: 0,
-        usage: 0,
-        endOfLife: 0
-    },
-    erf: {
-        manufacturing: 0,
-        installation: 0,
-        usage: 0,
-        endOfLife: 0
-    },
-    ase: {
-        manufacturing: 0,
-        installation: 0,
-        usage: 0,
-        endOfLife: 0
-    },
-    em: {
-        manufacturing: 0,
-        installation: 0,
-        usage: 0,
-        endOfLife: 0
-    }
-};
+  rc: {
+    manufacturing: 0;
+    installation: 0;
+    usage: 0;
+    endOfLife: 0;
+  };
+  erf: {
+    manufacturing: 0;
+    installation: 0;
+    usage: 0;
+    endOfLife: 0;
+  };
+  ase: {
+    manufacturing: 0;
+    installation: 0;
+    usage: 0;
+    endOfLife: 0;
+  };
+  em: {
+    manufacturing: 0;
+    installation: 0;
+    usage: 0;
+    endOfLife: 0;
+  };
+}
+
+export interface ProductImpact {
+  manufacturing: number;
+  installation: number;
+  usage: number;
+  endOfLife: number;
+  total: number;
+  product: AddProductType;
+  percentage: number;
+}
