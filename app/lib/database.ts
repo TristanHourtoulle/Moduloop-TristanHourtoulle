c7d80bd9fae102b86bdbc1953e8b8be9e8f3ee96
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-23 09:15:29 +0200

    fix: delete function tu update col updatedAt for bugs

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -27,19 +27,19 @@ const pool = new Pool({
 // );
 
 // Créer le trigger qui appelle cette fonction chaque fois qu'une ligne dans la table projects est mise à jour
-pool.query(
-  `
-    CREATE TRIGGER update_projects_modtime
-    BEFORE UPDATE ON projects
-    FOR EACH ROW
-    EXECUTE PROCEDURE update_modified_column();
-`,
-  (err, res) => {
-    if (err) {
-      console.error(err);
-    } else {
-    }
-  }
-);
+// pool.query(
+//   `
+//     CREATE TRIGGER update_projects_modtime
+//     BEFORE UPDATE ON projects
+//     FOR EACH ROW
+//     EXECUTE PROCEDURE update_modified_column();
+// `,
+//   (err: any, res: any) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//     }
+//   }
+// );
 
 export default pool;
