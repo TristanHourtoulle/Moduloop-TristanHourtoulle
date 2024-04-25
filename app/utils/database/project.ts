5d6656232e0f7883335c9124549b5814390b8555
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-25 11:29:29 +0200

    feat: add method to get project by project_id

--- app/utils/database/project.ts
+++ app/utils/database/project.ts
@@ -1,3 +1,5 @@
+import { databaseToSingleProjectModel } from "@utils/convert";
+
 export const getProjectsByUserId = async (id: number) => {
   const response = await fetch(`/api/project/list?id=${id}`, {
     method: "GET",
@@ -5,6 +7,19 @@ export const getProjectsByUserId = async (id: number) => {
   const data = await response.json();
   if (data.success && data.data.length > 0) {
     return data.data;
+  } else {
+    return null;
+  }
+};
+
+export const getProjectById = async (id: number) => {
+  const response = await fetch(`/api/project?id=${encodeURIComponent(id)}`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  if (data.success && data.data) {
+    const projectData = databaseToSingleProjectModel(data.product);
+    return projectData;
   } else {
     return null;
   }
