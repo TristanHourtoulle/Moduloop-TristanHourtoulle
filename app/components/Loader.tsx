02d6f9550490f806556516298ac482cd016b5f9f
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-04-25 12:07:15 +0200

    feat: update Loader comp with the real logo

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Image from 'next/image';
+import Image from "next/image";
 
 const Loader = () => {
-    return (
-        <div className='flex items-center justify-center my-auto'>
-            <Image
-                src="/icons/logoImage.svg"
-                alt="Loader"
-                width={50}
-                height={50}
-                className='loader'
-            />
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-center my-auto">
+      <Image
+        src="/icons/logoImage.png"
+        alt="Loader"
+        width={50}
+        height={50}
+        className="loader"
+      />
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
