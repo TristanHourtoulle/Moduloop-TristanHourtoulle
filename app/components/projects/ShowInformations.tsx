20cb84ab0986c634656c66e78b6c37b09628f883
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-05-10 11:27:10 +0200

    feat: add comp to diplay all info about a project and the possibility to udate all of that

---
+++ app/components/projects/ShowInformations.tsx
@@ -0,0 +1,211 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@components/features/Sheet";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@components/input/Select";
+
+import { Button } from "@components/button/Button";
+import { Input } from "@components/input/Input";
+import { Label } from "@components/input/Label";
+import { Textarea } from "@components/input/TextArea";
+import { getGroupsByUserId } from "@utils/database/group";
+import { updateAllFieldsInProject } from "@utils/database/project";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export type ShowInformationsProps = {
+  project: any;
+  ctaSave: () => void;
+};
+
+export const ShowInformations = ({
+  project,
+  ctaSave,
+}: ShowInformationsProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [userGroups, setUserGroups] = useState<any[]>([]);
+
+  const [selectedGroup, setSelectedGroup] = useState<string | null>(
+    project.groupInfo.id // Initialisez la valeur avec la valeur actuelle du projet
+  );
+  const [selectedName, setSelectedName] = useState<string | null>(
+    project.name // Initialisez la valeur avec la valeur actuelle du projet
+  );
+  const [selectedDescription, setSelectedDescription] = useState<string | null>(
+    project.description // Initialisez la valeur avec la valeur actuelle du projet
+  );
+
+  const [backupData, setBackupData] = useState<any>({
+    name: project.name,
+    description: project.description,
+    group: project.groupInfo.id,
+  });
+
+  const handleSaveChanges = async () => {
+    try {
+      setIsLoading(true);
+      const data = {
+        name: selectedName,
+        description: selectedDescription,
+        group_id: selectedGroup,
+      };
+      if (data.group_id === "-1") {
+        data.group_id = null;
+      }
+      console.log("data", data);
+      const newProject = {
+        ...project,
+        name: selectedName,
+        description: selectedDescription,
+        group: selectedGroup,
+      };
+      const response = await updateAllFieldsInProject(newProject);
+      if (!response) {
+        toast.error(
+          "Une erreur est survenue au moment de la sauvegarde. Veuillez réessayer."
+        );
+        setIsLoading(false);
+        return;
+      }
+      ctaSave();
+      setIsLoading(false);
+      toast.success("Les informations ont été sauvegardées avec succès.");
+    } catch (error: any) {
+      toast.error(
+        "Une erreur est survenue au moment de la sauvegarde. Veuillez réessayer."
+      );
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      console.log("project", project);
+      const idUser = project?.user_id;
+      console.log("idUser", idUser);
+
+      if (!idUser) return;
+      const data = await getGroupsByUserId(idUser);
+      console.log("data", data);
+      setUserGroups(data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const handleGroupChange = (event: React.ChangeEvent<HTMLButtonElement>) => {
+    setSelectedGroup(event.target.value);
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Image
+          src="/icons/Edit.svg"
+          alt="Change"
+          width={50}
+          height={50}
+          className="cursor-pointer hover:opacity-50 transition-opacity duration-200"
+        />
+      </SheetTrigger>
+
+      {!isLoading && (
+        <SheetContent side={"right"}>
+          <SheetHeader>
+            <SheetTitle>Informations du projet</SheetTitle>
+            <SheetDescription>
+              Faites des modifications sur les informations de votre projet ici.
+              Cliquez sur "sauvegarder" une fois que vous avez mis à jour vos
+              données.
+            </SheetDescription>
+          </SheetHeader>
+
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Nom
+              </Label>
+              <Input
+                id="name"
+                value={selectedName ?? ""}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setSelectedName(event.target.value)
+                }
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Textarea
+                id="description"
+                value={selectedDescription ?? ""}
+                className="col-span-3"
+                onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setSelectedDescription(event.target.value)
+                }
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="group" className="text-right">
+                Groupe
+              </Label>
+              <div style={{ width: "150px" }}>
+                <Select>
+                  <SelectTrigger
+                    className="w-full"
+                    onChange={(event: React.ChangeEvent<HTMLButtonElement>) =>
+                      handleGroupChange(event)
+                    }
+                  >
+                    <SelectValue placeholder={project.groupInfo.name} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value={"-1"}>Aucun Groupe</SelectItem>
+                    {userGroups.map((group) => (
+                      <SelectItem key={group.id} value={group.id}>
+                        {group.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+
+          <SheetFooter>
+            <SheetClose asChild>
+              {(backupData.name !== selectedName ||
+                backupData.description !== selectedDescription ||
+                backupData.group !== selectedGroup) && (
+                <Button
+                  variant="secondary"
+                  onClick={handleSaveChanges}
+                  image={null}
+                  size="small"
+                  content="Enregistrer"
+                  disabled={isLoading}
+                  moreClasses="ml-auto mr-auto"
+                />
+              )}
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      )}
+    </Sheet>
+  );
+};
