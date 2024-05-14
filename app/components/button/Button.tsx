23bc5cd619d1428850bf7004973a3e85ce0bcff3
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-05-14 12:06:02 +0200

    fix: issues with responsive

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -13,34 +13,23 @@ export type ButtonProps = {
 export const Button = (props: ButtonProps) => {
   const { variant, onClick, content, disabled, image, size, moreClasses } =
     props;
-  let className = `cursor-pointer transition-all duration-300 flex items-center gap-2`;
-  let imageClassName = { width: 20, height: 20 };
+
+  let className = `inline-block cursor-pointer transition-all duration-300 flex items-center gap-2`;
 
   // Variant Section
   if (variant === "primary") {
-    if (image) {
-      className += ` text-white bg-[#30C1BD] hover:bg-[#249C99]`;
-    } else {
-      className += ` text-white bg-[#30C1BD] border-2 border-[#F6F6F6] hover:bg-white hover:border-2 hover:border-[#30C1BD] hover:text-[#30C1BD]`;
-    }
+    className += ` text-white bg-[#30C1BD] hover:bg-[#249C99]`;
   } else if (variant === "secondary") {
-    if (image) {
-      className += ` text-[#30C1BD] bg-white border-2 border-[#30C1BD] hover:bg-primary-transparent hover:border-primary-border-transparent`;
-    } else {
-      className += ` text-[#30C1BD] bg-white border-2 border-[#30C1BD] hover:bg-[#30C1BD] hover:text-white`;
-    }
+    className += ` text-[#30C1BD] bg-white border-2 border-[#30C1BD] hover:bg-primary-transparent hover:border-primary-border-transparent`;
   }
 
   // Size Section
   if (size === "small") {
     className += ` px-3 py-2 rounded-md text-md gap-2`;
-    imageClassName = { width: 20, height: 20 };
   } else if (size === "medium") {
     className += ` px-4 py-2 rounded-md text-lg gap-3`;
-    imageClassName = { width: 23, height: 23 };
   } else if (size === "large") {
     className += ` px-6 py-3 rounded-lg text-xl gap-4`;
-    imageClassName = { width: 25, height: 25 };
   }
 
   // MoreClasses Section
@@ -53,8 +42,8 @@ export const Button = (props: ButtonProps) => {
       {image && (
         <Image
           src={image}
-          width={imageClassName.width}
-          height={imageClassName.height}
+          width={size === "small" ? 20 : size === "medium" ? 23 : 25}
+          height={size === "small" ? 20 : size === "medium" ? 23 : 25}
           alt="icon"
         />
       )}
