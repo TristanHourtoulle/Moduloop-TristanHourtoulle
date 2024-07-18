e6cfc1795827b98668d9be7bb472c9ca151422e4
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-07-18 17:32:07 +0200

    fix: toaster from sonner dn't be displayed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@components/Header";
 import { NextUIProvider } from "@nextui-org/react";
 import "@styles/globals.css";
 import type { Metadata } from "next";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "sonner";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -32,7 +32,12 @@ export default function RootLayout({
           <Header />
         </div>
         <div className="">
-          <Toaster />
+          <Toaster
+            richColors
+            position="top-center"
+            expand={false}
+            className="w-full text-xl"
+          />
           <NextUIProvider className="pb-10">{children}</NextUIProvider>
         </div>
       </body>
