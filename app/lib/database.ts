d6e2effa7cb3d6959d76fb895ed6db7ac41f89ff
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-20 10:03:24 +0800

    feat: update the process.env

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -1,10 +1,17 @@
 const { Pool } = require("pg");
 
+// Chargement des variables d'environnement depuis le fichier .env
+require("dotenv").config();
+
 const pool = new Pool({
-  host: "185.158.132.197",
-  user: "postgres",
-  password: "q3umdAVs4HIVZDOwkcXe",
-  database: "moduloop",
+  host: process.env.DB_HOST, // Hôte Supabase
+  user: process.env.DB_USER, // Utilisateur Supabase
+  password: process.env.DB_PASSWORD, // Mot de passe Supabase
+  database: process.env.DB_NAME, // Nom de la base de données Supabase
+  port: process.env.DB_PORT, // Port Supabase
+  ssl: {
+    rejectUnauthorized: false, // SSL requis pour Supabase
+  },
 });
 
 export default pool;
